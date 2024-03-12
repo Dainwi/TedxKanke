9d130b21fdf8747a2ee17058e8a3a0105c32938c
Dainwi Kumar <danwichoudhary@gmail.com>
2024-03-12 11:11:09 +0530

    navbar updated

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 document.getElementById('menu-toggle').addEventListener('click', function () {
     var mobileMenu = document.getElementById('mobile-menu');
-    if (mobileMenu.style.display === 'block') {
+    if (mobileMenu.style.display === 'block' || mobileMenu.style.display === '') {
         mobileMenu.style.display = 'none';
     } else {
         mobileMenu.style.display = 'block';
     }
 });
 
-// Close dropdown when clicking outside
+// Close menu
 document.addEventListener('click', function (e) {
     var mobileMenu = document.getElementById('mobile-menu');
     var menuToggle = document.getElementById('menu-toggle');
@@ -16,41 +16,16 @@ document.addEventListener('click', function (e) {
     }
 });
 
+// Toggle dropdown menu on hover
+document.querySelectorAll('.dropdown').forEach(dropdown => {
+    const button = dropdown.querySelector('button');
+    const menu = dropdown.querySelector('.dropdown-menu');
 
+    dropdown.addEventListener('mouseenter', () => {
+        menu.classList.remove('hidden');
+    });
 
-// contact us form modal 
-const openModalButton = document.getElementById('openModal');
-const closeModalButton = document.getElementById('closeModal');
-const modal = document.getElementById('modal');
-
-openModalButton.addEventListener('click', () => {
-    modal.style.display = 'block';
-    modal.classList.add('fade-in');
-});
-
-closeModalButton.addEventListener('click', () => {
-    modal.classList.remove('fade-in');
-    modal.classList.add('fade-out');
-    setTimeout(() => {
-        modal.style.display = 'none';
-        modal.classList.remove('fade-out');
-    }, 500); // Change 500 to match animation duration
-});
-
-// got to top button
-const goToTopBtn = document.getElementById("goToTopBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        goToTopBtn.style.display = "block";
-    } else {
-        goToTopBtn.style.display = "none";
-    }
-};
-
-// When the user clicks on the button, scroll to the top of the document
-goToTopBtn.addEventListener("click", function () {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+    dropdown.addEventListener('mouseleave', () => {
+        menu.classList.add('hidden');
+    });
 });
